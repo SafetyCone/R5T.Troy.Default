4d28050c9d1e92e48d7686ef77310cdb6b21ec61
MinexAutomation <minex.automation@gmail.com>
2020-01-22 16:46:49 -0800

    Added default TemporaryDirectoryNameConvention implementation of ITemporaryDirectoryNameConvention service.

--- source/R5T.Troy.Default/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Troy.Default
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Troy.Default/Code/Services/Implementations/TemporaryDirectoryNameConvention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Troy.Default
+{
+    public class TemporaryDirectoryNameConvention : ITemporaryDirectoryNameConvention
+    {
+        public const string TemporaryDirectoryName = "Temp";
+
+
+        public string GetTemporaryDirectoryName()
+        {
+            return TemporaryDirectoryNameConvention.TemporaryDirectoryName;
+        }
+    }
+}
