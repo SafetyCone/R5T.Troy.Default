f8df697f4a73301fe17b9a0ff4450ce8af8bf2d6
MinexAutomation <minex.automation@gmail.com>
2020-01-30 01:36:39 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Troy.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Troy.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="TemporaryDirectoryNameConvention"/> implementation of <see cref="ITemporaryDirectoryNameConvention"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultTemporaryDirectoryNameConvention(this IServiceCollection services)
+        {
+            services.AddSingleton<ITemporaryDirectoryNameConvention, TemporaryDirectoryNameConvention>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="TemporaryDirectoryNameConvention"/> implementation of <see cref="ITemporaryDirectoryNameConvention"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ITemporaryDirectoryNameConvention> AddDefaultTemporaryDirectoryNameConventionAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ITemporaryDirectoryNameConvention>(() => services.AddDefaultTemporaryDirectoryNameConvention());
+            return serviceAction;
+        }
+    }
+}
