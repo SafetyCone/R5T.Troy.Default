0d85af10390aecf6b54badb966764bb32af06b75
davidcoats <david@getsafetycone.com>
2022-04-27 15:37:16 -0700

    Interim changes.

--- source/R5T.Troy.Default/Code/Services/Implementations/TemporaryDirectoryNameConvention.cs
+++ source/R5T.Troy.Default/Code/Services/Implementations/TemporaryDirectoryNameConvention.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Troy.Default
-{
-    public class TemporaryDirectoryNameConvention : ITemporaryDirectoryNameConvention
+{[ServiceImplementationMarker]
+    public class TemporaryDirectoryNameConvention : ITemporaryDirectoryNameConvention,IServiceImplementation
     {
         public const string TemporaryDirectoryName = "Temp";
 
